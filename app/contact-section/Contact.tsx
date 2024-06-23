b70e2807bfd658c6ee611ee8e03907e58fdcab9d
Draxler <76547508+hengthegoat@users.noreply.github.com>
2024-06-24 01:36:18 +0700

    Update Contact.tsx

--- app/contact-section/Contact.tsx
+++ app/contact-section/Contact.tsx
@@ -45,7 +45,7 @@ const Contact = () => {
             />
             <div className="flex items-center w-[298px] md:w-[335px] bor der gap-1 md:gap-2.5">
               <Link
-                href="mailto:victor.will.chuk@gmail.com?subject=Lets%20work%20together!&amp;body=Hello%2C%20I%20think%20we%20need%20you%20to%20work%20on%2Fcollaborate%20this%20particular%20product...%20Reach%20out%20as%20soon%20as%20you%20can."
+                href="mailto:heng@horizxon.tech?subject=Lets%20work%20together!&amp;body=Hello%2C%20I%20think%20we%20need%20you%20to%20work%20on%2Fcollaborate%20this%20particular%20product...%20Reach%20out%20as%20soon%20as%20you%20can."
                 target="_blank"
                 aria-label="Send me an email"
                 className="mt-1 w-[147px] flex-1 underline underline-offset-2 hover:no-underline sm:mt-2 sm:w-[170px] md:mt-3 bor der lg:mt-4"
@@ -53,25 +53,25 @@ const Contact = () => {
                 <AnimatedBody text={"Send me an email"} className={"w-[190px] bor der md:w-[170px] pr-[40px] md:pr-0"} />
               </Link>
               <AnimatedBody
-                text={"or"}
+                text={"&"}
                 className={
                   "-mb-1 inline-block overflow-hidden bor der ml-2 md:-ml-[8px] sm:-mb-2 md:-mb-3 lg:-mb-4"
                 }
               />
               <Link
-                href="https://calendly.com/victor-will-chuk/discovery-call-frontend-dev"
+                href="https://horizxon.tech"
                 target="_blank"
                 aria-label="Send me an email"
                 className="mt-1 w-[110px] flex-1 underline underline-offset-2 hover:no-underline sm:mt-2 sm:w-[147px] md:mt-3 md:w-[120px] md:-ml-[3px] bor der lg:mt-4"
               >
-                <AnimatedBody text={"Book a call"} className={"w-[110px] md:w-[120px]"} />
+                <AnimatedBody text={"HorizxonLimited"} className={"w-[110px] md:w-[120px]"} />
               </Link>
             </div>
           </div>
 
           <div className="flex gap-10 text-[16px] font-bold text-[#e4ded7]  sm:gap-14 sm:text-[24px] md:gap-10 md:text-[16px] lg:gap-20 lg:text-[28px]">
             <Link
-              href="https://github.com/victorcodess"
+              href="https://github.com/HenGPlayZ"
               target="_blank"
               aria-label="View GitHub Profile"
             >
@@ -85,7 +85,7 @@ const Contact = () => {
               />
             </Link>
             <Link
-              href="https://www.linkedin.com/in/victor-williams-chukwudi/"
+              href="https://www.linkedin.com/in/tang-heng-1663432a5/"
               target="_blank"
               aria-label="View LinkedIn Profile"
             >
@@ -99,7 +99,7 @@ const Contact = () => {
               />
             </Link>
             <Link
-              href="https://twitter.com/victorwill__"
+              href="https://x.com/stuvythegoat"
               target="_blank"
               aria-label="View Twitter Profile"
             >
@@ -113,7 +113,7 @@ const Contact = () => {
               />
             </Link>
             <Link
-              href="https://www.instagram.com/victorwill__/"
+              href="https://www.instagram.com/hengthegoat/"
               target="_blank"
               aria-label="View Instagram Profile"
             >
@@ -127,7 +127,7 @@ const Contact = () => {
               />
             </Link>
             <Link
-              href="https://blog.victorwilliams.me/"
+              href="https://horizxon.tech/"
               target="_blank"
               aria-label="View Hashnode Profile"
             >
