d8bb086d4ae837341a075a838038f2eeb11cd167
Draxler <76547508+hengthegoat@users.noreply.github.com>
2024-06-24 01:55:00 +0700

    Update BlogGrid.tsx

--- app/blog-section/BlogGrid.tsx
+++ app/blog-section/BlogGrid.tsx
@@ -5,7 +5,17 @@ import { monaSans } from "../fonts/monaSans";
 import AnimatedBody from "../animations/AnimatedBody";
 
 const Blog = () => {
-
+  return (
+    <section className="">
+      <div
+        className={``}
+      >
+        {" "}
+      </div>
+      <div className="">
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
